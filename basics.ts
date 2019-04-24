cee6f3205acaf932da4a290d2f92c4868c5ef39e
PrzemekGdula <przemo.g12@wp.pl>
2019-04-24 12:09:48 +0200

    basic types and ANY

---
+++ basics.ts
@@ -0,0 +1,36 @@
+let zrobione: boolean = true
+let liczba: number = 123
+let text: string = 'To jest test'
+let wnioskowanie = 'Test inferencji'
+//wnioskowanie = 123
+
+const tablicaLiczb: number[] = [1, 2, 3]
+const tablicaLiczb2: Array<number> = tablicaLiczb
+
+const tablicaStringow: Array<string> = ['ok', 'test']
+const TablicaStringow2: string[] = tablicaStringow
+
+let krotka: [number, boolean] = [1, false];
+// krotka = ['asd', 'asd']
+let krotka1: [string, boolean[]] = ['test', [true, false]]
+let krotka3: [[number, boolean], [string, boolean[]]] = [krotka, krotka1]
+
+let miesiacRezerwacji: string = 'lipiec'
+miesiacRezerwacji = 'june'
+
+if (miesiacRezerwacji === 'lipiec') {
+    //fuckup
+}
+
+enum Miesiac { STYCZEN, LUTY, MARZEC, KWIECIEN, MAJ, CZERWIEC, LIPIEC, SIERPIEN, WRZESIEN, PAZDZIERNIK, LISTOPAD }
+
+let newMiesiacRezerwacji: Miesiac = Miesiac.LIPIEC
+
+if (newMiesiacRezerwacji === Miesiac.LIPIEC) {
+    console.log('sukces')
+}
+
+let jeszczeNieWiemy: any = {}
+jeszczeNieWiemy = 123
+jeszczeNieWiemy = krotka3
+jeszczeNieWiemy = Miesiac.KWIECIEN
